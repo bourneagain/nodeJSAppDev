5590c71f055290f6ad7ae2b90d9c8085c6ca67db
Shyam Rajendran <samateapple@gmail.com>
2014-10-25 02:50:04 -0500

    added views and mongdo connection

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
 var http = require('http'),
-    path = require('path'),
-    express = require('express');
+    express = require('express'),
+    path = require('path');
 
+MongoClient = require('mongodb').MongoClient,
+Server = require('mongodb').Server,
+CollectionDriver = require('./collectionDriver').CollectionDriver;
+ 
 var app = express();
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000); 
+app.set('views', path.join(__dirname, 'views')); //A
+app.set('view engine', 'jade'); //B
+
+
+var mongoHost = 'localHost'; //A
+var mongoPort = 27017; 
+var collectionDriver;
+ 
+var mongoClient = new MongoClient(new Server(mongoHost, mongoPort)); //B
+mongoClient.open(function(err, mongoClient) { //C
+  if (!mongoClient) {
+         console.error("Error! Exiting... Must start MongoDB first");
+	       process.exit(1); //D
+   }
+   var db = mongoClient.db("MyDatabase");  //E
+   collectionDriver = new CollectionDriver(db); //F
+});
+ 
 app.use(express.static(path.join(__dirname, 'public')));
+ 
 app.get('/', function (req, res) {
-	  res.send('<html><body><h1>Hello World</h1></body></html>');
+  res.send('<html><body><h1>Hello World</h1></body></html>');
 });
-app.get('/:a?/:b?/:c?', function (req,res) {
-		res.send(req.params.a + ' ' + req.params.b + ' ' + req.params.c);
+ 
+app.use(function (req,res) {
+    res.render('404', {url:req.url});
 });
-app.use(function (req,res) { //1
-    res.render('404', {url:req.url}); //2
-});
+ 
 http.createServer(app).listen(app.get('port'), function(){
-	  console.log('Express server listening on port ' + app.get('port'));
+  console.log('Express server listening on port ' + app.get('port'));
 });
